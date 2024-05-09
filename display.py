2729542c93952024dca0c270a05d906478e2f426
NateSiwel <etanlew@gmail.com>
2024-05-09 12:06:23 -0700

    normalize

--- display.py
+++ display.py
@@ -1,21 +1,23 @@
 import cv2
+import numpy as np
 
 class Display(object):
-    def __init__(self):
+    def __init__(self, W, H):
         self.window_name = 'frame'
         cv2.namedWindow(self.window_name, cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(self.window_name, W, H)
 
     def show(self, frame):
         cv2.imshow(self.window_name, frame)
 
-    def draw_keypoints(self, frame, kps, matches):
+    def draw_keypoints(self, frame, kps, matches, denormalize):
         #cv2.drawKeypoints(frame, kps, frame, color=(255,0,255), flags=0)
         current_coords = [match[0] for match in matches]
         old_coords = [match[1] for match in matches]
 
         for i in range(len(current_coords)):
-            current_point = current_coords[i]
-            old_point = old_coords[i]
+            current_point = denormalize(current_coords[i])
+            old_point = denormalize(old_coords[i])
 
             current_x = int(current_point[0])
             current_y = int(current_point[1])
@@ -24,9 +26,7 @@ class Display(object):
 
             cv2.line(frame, (current_x, current_y), (old_x, old_y), color=(0, 255, 0), thickness=2)
 
-        
-        #cv2.drawKeypoints(frame, kps, frame, color=(255,255,255), flags=0)
-
+            #cv2.drawKeypoints(frame, kps, frame, color=(255,255,255), flags=0)
 
         return frame 
 
--- orbs.py
+++ orbs.py
@@ -1,13 +1,23 @@
 import cv2
 from display import Display
 from featureExtractor import FeatureExtractor 
+import numpy as np
 
 #vid = cv2.VideoCapture(-1)
 vid = cv2.VideoCapture('driving.mp4')
 vid.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 
-display = Display()
-orb = FeatureExtractor()
+W = 1920//2
+H = 1080//2
+
+F = 1
+display = Display(W, H)
+
+#camera matrix
+K = np.array([[F, 0, W//2], [0, F, H//2], [0, 0, 1]])
+
+
+orb = FeatureExtractor(K)
 
 def process_frame(frame):
     matches = orb.extract(frame)
@@ -18,7 +28,7 @@ def process_frame(frame):
     des = last['des']
     
     if kps is not None:
-        frame = display.draw_keypoints(frame, kps, matches)
+        frame = display.draw_keypoints(frame, kps, matches, orb.denormalize)
 
     return frame
 
